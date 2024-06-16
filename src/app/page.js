de78ad266baeacb35384936b6421c2b4398690ce
NabilaAzzahrra <zahrrranabill11@gmail.com>
2024-06-16 22:04:33 +0700

    deploy

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <div className="">PORTFOLIO-NABILA AZZAHRA</div>
+        <div className="">PORTFOLIO-NABILA AZZAHRAAAAA</div>
     </main>
   );
 }
